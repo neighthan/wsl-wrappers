06c96c4892c0d19f5fec910b372a0e9bb3849220
Nathan Hunt <neighthan.hunt@gmail.com>
2019-02-22 12:35:20 -0500

    Scripts to install + use the `wslgit` wrapper.

--- wsl_wrappers/scripts_api.py
+++ wsl_wrappers/scripts_api.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+from subprocess import run
 from .utils import run_command
 from ._scripts.pdf2svg import get_command_pdf2svg
 from ._scripts.pdflatex import get_command_pdflatex
+from ._scripts.install_wsl_git import install_wsl_git
 
 
 def pdf2svg(verbose: bool = False):
@@ -15,3 +18,12 @@ def pdflatex(verbose: bool = False):
     if verbose:
         print(cmd)
     run_command(cmd)
+
+
+def git(verbose: bool = False):
+    git_cmd_path = Path(__file__).parent / "_scripts" / "git.cmd"
+    run(str(git_cmd_path), shell=True)
+
+
+def install_wgit(verbose: bool = False):
+    install_wsl_git(verbose=verbose)
